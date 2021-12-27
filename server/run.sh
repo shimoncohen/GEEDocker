5d69147da9c90c77ecd2d45a640287af8da8cdaa
shimoncohen <shimoncohen655@gmail.com>
2021-12-27 17:04:58 +0200

    chore: add host flag to server run script

--- server/run.sh
+++ server/run.sh
@@ -5,21 +5,53 @@ STORAGE_MOUNT="source=gee-server-storage,target=/var/opt/google/pgsql/data"
 
 # Default values of arguments
 ENTERYPOINT=0
+HOST=""
 
 # Loop through arguments and process them
 # Taken from: https://pretzelhands.com/posts/command-line-flags
-for arg in "$@"
-do
+for arg in "$@"; do
     case $arg in
-        --entrypoint)
+    --entrypoint)
         ENTERYPOINT=1
         shift # Remove
+        ;;
+    --host=*)
+        HOST="${arg#*=}"
+        shift # Remove
+        ;;
+    --host)
+        HOST="$2"
+        shift # Remove
+        shift
         ;;
     esac
 done
 
+ATTACH_DOCKER_FLAG="-d"
+ATTACH_HOST_FLAG=""
+
+docker_command_str="docker run \
+    %s \
+    %s \
+    -v $VOLUME_CONNECT \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --mount $STORAGE_MOUNT \
+    %s \
+    --name servercontainer \
+    -p 8081:80 \
+    geeserver-slim:v1"
+
+docker_command=$(printf "$docker_command_str" "$ATTACH_DOCKER_FLAG" "$ATTACH_HOST_FLAG" "")
+
+# Edit entrypoint if requested
 if [ $ENTERYPOINT -eq 1 ]; then
-    docker run --rm -it --entrypoint /bin/bash --privileged -v $VOLUME_CONNECT --mount $STORAGE_MOUNT --name servercontainer -p 8081:80 geeserver:v1
-else
-    docker run --rm --privileged -v $VOLUME_CONNECT --mount $STORAGE_MOUNT --name servercontainer -p 8081:80 geeserver:v1 &
+    ATTACH_DOCKER_FLAG="--entrypoint /bin/bash -it"
 fi
+
+# Edit host if requested
+if [ ! -z $HOST ]; then
+    ATTACH_HOST_FLAG="-h $HOST"
+fi
+
+docker_command=$(printf "$docker_command_str" "$ATTACH_DOCKER_FLAG" "$ATTACH_HOST_FLAG" "$PGVOL")
+$docker_command
